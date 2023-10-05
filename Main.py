3b91cd3c054637023644de1050cd3f893a2d9e7b
Mihir Panjikar <mihirpanjikar@gmail.com>
2023-10-05 12:45:42 -0700

    Not completely functional, but able to ocr each individual cells now

--- Main.py
+++ Main.py
@@ -2,109 +2,107 @@ import cv2
 import pytesseract
 import tkinter as tk
 from tkinter import filedialog
+import time
 
+# Func for opening the dialog box for inserting the image
 def open_file_dialog():
     file_path = filedialog.askopenfilename()
     if file_path:
         label.config(text="Selected File: " + file_path)
         processing_image(file_path)
 
+# Func to stop the program 
+def kill_process(error: str):
+    label.config(text=error)
+    time.sleep(3)
+    cv2.destroyAllWindows()
+    root.destroy()
+
+# Func to OCR the image
 def processing_image(filepath):
     # Loading the table image
     image = cv2.imread(filepath)
 
     # Check if the image was loaded successfully
-    if image is None:
-        print(f"Error: Unable to load the image from '{image}'")
-    else:
-        # Preprocess the image (you can adjust these preprocessing steps as needed)
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+    if image is None or not str(filepath).endswith(('.png', '.jpeg', '.jpg')):
+        error_msg = f"Error: Unable to load the image from '{image}'"
+        kill_process(error_msg)
 
-        # Find contours in the image to identify table cells
-        contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    else:
+        res = cv2.resize(image,None,fx=0.8, fy=0.8, interpolation = cv2.INTER_CUBIC)
+        h,w,ch = res.shape
+        cv2.rectangle(res, (0,0), (w,h), (0,0,0), 10)
+
+        gray = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
+        _, thresh = cv2.threshold(gray,220,255,cv2.THRESH_BINARY)
+        contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
+        sort_cnts = sorted(contours, key=lambda ctr: cv2.boundingRect(ctr)[0] + cv2.boundingRect(ctr)[1] * res.shape[1])
+        
+        if len(sort_cnts) < 1:
+            error_msg = "Error, OCR failed"
+            kill_process(error_msg)
+
+        ROIs = []
+        coordinates = []
+
+        for cnt in sort_cnts:
+            x,y,w,h = cv2.boundingRect(cnt)
+            if [x, y, w, h] not in coordinates:
+                if 2000 > w > 70 and h < 100:
+                    ROI = res[y:y+h, x:x+w]
+                    ROIs.append(ROI)
+                    cv2.rectangle(res, (x,y), (x+w,y+h), (0,255,0), 2)
+            coordinates.append([x, y, w, w])
+            
+        # Optionally, you can save the preprocessed image
+        cv2.imwrite('preprocessed_table.png', res)
 
         # Loop through each contour (cell) and extract text
-        for contour in contours:
-            x, y, w, h = cv2.boundingRect(contour)
-            cell = image[y:y+h, x:x+w]
-
+        for ROI in ROIs:
             # Perform OCR on the cell
-            text = pytesseract.image_to_string(cell)  # --psm 6 for treating the image as a single uniform block of text
+            text = pytesseract.image_to_string(ROI)  # 8 Treat the image as a single word.
             
             for char in text:
-                text = text.replace("|", "")
+                if ord(char) not in range(65,91) and ord(char) not in range(97, 123) and ord(char) not in range(48, 58):
+                    text = text.replace(char, "")
             
-            newline_separated = text.split('\n')
+            text = text.strip()
             
-            filtered_list = []
-            for item in newline_separated:
-                if not item.strip() == '':
-                    filtered_list.append(item)
-
             # Print the extracted text from the cell
-            print()
-            print(text.strip())
-            
-            print()
+            print(text)
             # print(filtered_list)
             
-            # Gets the table fields for creating the table
-            table_fields = filtered_list[0].split(' ')
+            # # Gets the table fields for creating the table and filters them
+            # table_fields = filtered_list[0].split(' ')
+            # while '' in table_fields:
+            #     table_fields.remove('')
             
-            # Gets the table records
-            records = filtered_list[1:]
+            # # Gets the table records
+            # records = filtered_list[1:]
             
-            while '' in table_fields:
-                table_fields.remove('')
-            
-            # print(table_fields)
-            
-            # W
-            with open("main_sql.sql", "w") as file:
-                # Creating the table
-                file.write("CREATE TABLE 'table_name' (")
-                for item in table_fields:
-                    if item != table_fields[-1]:
-                        file.write(item + " type" + ",")
-                    
-                    else:
-                        file.write(item + " type")
-                file.write(");")
-                
+            # # Writing the sql file
+            # with open("main_sql.sql", "w") as file:
+            #     # Creating the table
+            #     create_table = f"CREATE TABLE 'table_name' ({' type, '.join(table_fields)} type);\n\n"
+            #     file.write(create_table)                
 
-                # Writing Insert statements
-                file.write("\n\nINSERT INTO 'table_name'\n\tVALUES (")
-                for record in records:
-                    individual_entities = record.split(' ')
-                    while '' in individual_entities:
-                        individual_entities.remove('')
+            #     # Writing Insert statements
+            #     file.write("INSERT INTO 'table_name'\n\tVALUES (")
+            #     for record in records:
+            #         if record != records[-1]:
+            #             file.write(f"{', '.join(record)}),\n\t\t(")
                         
-                    if record != records[-1]:
-                        for entity in individual_entities:
-                            if entity != individual_entities[-1]:
-                                file.write(entity + " ,")
-                            else:
-                                file.write(entity + "),\n\t\t(")
-                    else:
-                        for entity in individual_entities:
-                            if entity != individual_entities[-1]:
-                                file.write(entity + " ,")
-                            else:
-                                file.write(entity + ");")
-                    
-                file.close()
+            #         else:
+            #             file.write(f"{', '.join(record)});")
 
-        # Optionally, you can save the preprocessed image
-        # cv2.imwrite('preprocessed_table.png', thresh)
+            #     file.close()
 
-        # Release resources
+    # Release resources
     cv2.destroyAllWindows()
-    
     root.destroy()
 
 # Create a Tkinter window
-root = tk.Tk()  # Replace "plastik" with the desired theme
+root = tk.Tk()
 root.title("IMG TO SQL")
 root.geometry("600x400")
 
--- main_sql.sql
+++ main_sql.sql
@@ -1,10 +1,4 @@
-CREATE TABLE 'table_name' (empld type,ename type,city type,salary type,Join_date type,department type);
+CREATE TABLE 'table_name' (Finance type);
 
 INSERT INTO 'table_name'
-	VALUES (101 ,Pune ,35000 ,HR),
-		(102 ,Bangalore ,50000 ,Sales),
-		(103 ,Mumbai ,12000 ,HR),
-		(104 ,Delhi ,18000 ,Finance),
-		(105 ,Delhi ,60000 ,Marketing),
-		(106 ,Pune ,45000 ,Accounts),
-		(107 ,Hyderabad ,20000 ,Finance);
+	VALUES (
